54db77c46d6c488aa62939edd91448f402766ed3
Luis <luisangelcastromugno24@gmail.com>
2026-07-11 20:53:31 -0500

    base-v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders} from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -7,4 +7,20 @@ export default defineConfig({
   vite: {
     plugins: [tailwind()],
   },
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Roboto',
+      cssVariable: '--font-roboto',
+      weights: ['500', '700']
+    },
+    {
+      provider: fontProviders.google(),
+      name: 'Montserrat',
+      cssVariable: '--font-montserrat',
+      weights: ['400', '500', '600', '700']
+    }
+
+  ],
 });
